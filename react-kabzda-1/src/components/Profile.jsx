08d17c35adbf4d1c60d5ceaae6db5675a016b767
Petr <feelinhush01@gmail.ru>
2021-10-06 08:27:49 +0300

    add commponents

---
+++ react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Pokai_Bucht_auf_Oahu.jpg/1200px-Pokai_Bucht_auf_Oahu.jpg' />
+        <div>
+          ava + disciption 
+        </div>
+        <div>
+          my posts
+          <div>
+            New posts
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
